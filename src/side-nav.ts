f5124ea3afed9a9b042ecaeca672161b9bce7392
Jessica Chen <jessica.scoltock@protonmail.com>
2023-08-21 16:16:25 +0100

    Make side menu toggle-able

--- src/side-nav.ts
+++ src/side-nav.ts
@@ -1,24 +1,57 @@
 import { LitElement, css, html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
+import menu from "./assets/menu.svg";
+import close from "./assets/close.svg";
 
 @customElement("side-nav")
 export class SideNav extends LitElement {
+  @property() open = false;
+
+  toggleNav() {
+    this.open = !this.open;
+  }
+
   render() {
-    return html`<div>
-      <ul>
+    return html`<div id="menu-wrapper" class="${this.open ? "open" : "closed"}">
+      <ul class="menu-items">
         <slot></slot>
       </ul>
+      <button class="toggle-nav" @click=${this.toggleNav}>
+        <img src="${this.open ? close : menu}" width="14" />
+      </button>
     </div>`;
   }
 
   static styles = css`
-    :host {
+    #menu-wrapper {
       height: 100vh;
-      width: 250px;
       position: absolute;
       top: 0;
       left: 0;
+    }
+
+    .menu-items {
+      height: 100%;
+      width: 250px;
       overflow-y: scroll;
+      float: left;
+      margin-top: 0;
+      padding-top: 16px;
+      background: rgba(255, 255, 255, 0.5);
+    }
+
+    .toggle-nav {
+      float: left;
+      width: 26px;
+      margin-left: 4px;
+    }
+
+    .toggle-nav > img {
+      margin-top: 4px;
+    }
+
+    #menu-wrapper.closed {
+      left: -250px;
     }
 
     ul {
